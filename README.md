b5285d2ed0817a8641ffa3fef184f24a6560818f
jodros <15092760+jodros@users.noreply.github.com>
2024-05-21 14:53:25 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# config.sile
+
+The support for configuration files was originally made for my first attempt to create a module for SILE, the now archived repo [extend.sile](https://github.com/jodros/extend.sile)... 
+
+The idea is to pass any options, settings and even framesets through a `toml` file. Actually, two files, the default being stored in `~/.config/sile/default.toml`, and the second one in user's working directory, called `settings.toml`, which could overwrite anything in the default.
+
+The basic structure is:
+
+```toml
+[options]
+papersize = "a4"
+
+[settings]
+document.language = "en"
+
+[scratch]
+
+[frames]
+
+```
+
+The `options` table refers only to **class options** and in the same way `settings` has to do only with declared settings, e.g. `document.parskip`... The scratch table could be used when one is creating a command, lets say it's called `chapter`, and wants to pass the options like this:
+
+```toml
+[scratch.chapter]
+indent = true # for the first paragraph
+case = "uppercase"
+```
+
+And then one could access these values using `SILE.scratch.chapter[option]`...
+
+About the frames, an actual example is:
+
+```toml
+[frames.right.content]
+left = "(100%pw-50%ph)/2"
+right = "100%pw-((100%pw-50%ph)/2)"
+top = "(100%ph-100%pw)/2"
+bottom = "top(footnotes)-1%ph"
+[frames.right.folio]
+left = "left(content)"
+right = "right(content)"
+top = "bottom(footnotes)+3%ph"
+bottom = "bottom(footnotes)+5%ph"
+[frames.right.footnotes]
+left = "left(content)"
+right = "right(content)"
+bottom = "90%ph"
+height = "0"
+```
+
+So one could call `SILE.scratch.frames.right` when creating a new master.
+
+## TODO
+
+- add support to `yaml` and others...
+
+## Installing
+
+Just run `luarocks install --server=https://luarocks.org/dev config.sile`, or clone this repository and then run `luarocks --local make`. LuaRocks will automatically install the dependencies if you don't have them yet...
+
---
+++ core/gatherer.lua
@@ -0,0 +1,89 @@
+local lfs = require "lfs"
+local toml = require 'toml'
+local datafile = require "datafile"
+
+toml.strict = false -- to enable more lua-friendly features (like mixed arrays)
+
+local function fileExist(f)
+    if io.open(f) ~= nil then
+        return true
+    else
+        return false
+    end
+end
+
+local function super(file, typeofreturn) --  parses toml files
+    local raw
+
+    if type(file) == "userdata" then
+        raw = file:read("*all")
+    elseif fileExist(file) and type(file) == "string" then
+        raw = assert(io.open(tostring(file), "r")):read("*all")
+    end
+
+    if typeofreturn == "string" then -- to show it as verbatim in the documentation
+        string.gsub(raw, "\n", "\n\n\n")
+        return raw
+    end
+
+    local status
+    local input, output = {}, {}
+
+    status, input = pcall(toml.parse, raw)
+
+    if status then
+        for i, j in pairs(input) do
+            output[i] = j
+        end
+    end
+
+    return output
+end
+
+local function merge(fallback, localfile, count) -- it runs through both files and compare each item at the lowest level
+    local T, count = fallback, count or 0
+
+    if localfile == nil or localfile == false then
+        return fallback
+    end
+
+    for key, value in pairs(fallback) do -- overwrites any value declared in the localfile
+        if type(value) == "table" and localfile[key] then
+            T[key], count = merge(T[key], localfile[key], count)
+        elseif localfile[key] ~= nil and localfile[key] ~= "" then
+            T[key] = localfile[key]
+        elseif localfile[key] == nil then
+            T[key] = fallback[key]
+        end
+    end
+
+    for key, value in pairs(localfile) do -- writes anything else from localfile which is not in default
+        if not fallback[key] then
+            T[key] = value
+        end
+    end
+
+    return T, count + 1
+end
+
+local function geToml()
+    local home, config, layoutConfig = os.getenv("HOME"), {}, {}
+    local dotsile = home .. "/.config/sile/"
+    local default = datafile.open("config/default.toml")
+    local locDefault = dotsile .. "default.toml"
+    local settings = lfs.currentdir() .. "/settings.toml"
+
+    if not fileExist(dotsile) then  lfs.mkdir(dotsile) end
+    if not fileExist(dotsile.."default.toml") then os.execute("cp " .. datafile.path("config/default.toml") .. " " .. dotsile) end
+
+    config = merge(super(default), super(locDefault))
+
+    if fileExist(settings) then
+        config = merge(config, super(settings))
+    end
+
+    return config
+end
+
+return { merge = merge, geToml = geToml }
+
